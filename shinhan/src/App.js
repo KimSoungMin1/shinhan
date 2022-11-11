6033363d5b81d42ea7e15b7bffd5d153584659d0
KimSoungMin1 <tjdals247470@naver.com>
2022-11-11 14:38:56 +0900

    배포

--- shinhan/src/App.js
+++ shinhan/src/App.js
@@ -1,28 +1,26 @@
-
-import './App.css';
-import Layout from './page/Layout';
-import Main from './components/Main/Main';
-import Card from './page/Card';
-import Login from './page/Login';
+import "./App.css";
+import Layout from "./page/Layout";
+import Main from "./components/Main/Main";
+import Card from "./page/Card";
+import Login from "./page/Login";
 import { DataProvider } from "./context/CardContext";
 import { Routes, Route } from "react-router-dom";
-import { BrowserRouter } from 'react-router-dom'
-
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <DataProvider>
         <Routes>
-            <Route path='/' element={<Layout/>}>
-              <Route index element={<Main/>}></Route>
-              <Route path='/c' element={<Card/>}></Route>
-              <Route path='/login' element={<Login/>}></Route>
-            </Route>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Main />}></Route>
+            <Route path="/c" element={<Card />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+          </Route>
         </Routes>
-      </DataProvider> 
+      </DataProvider>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
